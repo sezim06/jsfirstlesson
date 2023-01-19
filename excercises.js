17aeae02aadc68cf6bafdd149076afdf7c999f16
sezim06 <sezimaliaskarova352@gmail.com>
2023-01-20 00:46:58 +0600

    added  excercises.js

---
+++ excercises.js
@@ -0,0 +1,39 @@
+//example1
+let height = 5;
+let width = 8;
+let triangleArea = height * width / 2;
+
+console.log("площадь равняется " + triangleArea);
+
+//example2
+let hours = 56;
+let minutes = hours * 60;
+console.log("минуты 56 часов " + minutes);
+
+//example3
+let workHours = 8;
+let workPrise = 50;
+let workDays = 8;
+let result = workHours * workPrise * workDays;
+console.log("праграмист заработал " + result) 
+
+//example4
+let kofee = 15;
+let leave = 1566 / 100 * 15;
+console.log("чаевых " + leave);
+
+//example5
+let BasketballPlayer = 8 - 6; 
+console.log("баскетбольная " + BasketballPlayer); 
+
+ //example6
+let tractor = 22; 
+let example7 = 22 * 100; 
+console.log("трактор " + example7); 
+
+//example7
+let ferme = 12; 
+let example8 = 5 * 6 * 12; 
+console.log("ferme " + example8)
+
+
